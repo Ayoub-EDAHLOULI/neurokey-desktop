c033f579b0778fe6e8e79ea695ad3522eb396236
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2026-04-15 22:00:19 +0100

    Add a temporary main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import "./App.css";
+import SidebarLayout from "./layouts/SidebarLayout";
 
 function App() {
   return (
-    <main>
-      <h1>Welcome to Tauri + React</h1>
-    </main>
+    <SidebarLayout>
+      <div className="flex-1 flex items-center justify-center">
+        <div className="text-center">
+          <h2 className="text-2xl font-bold text-text mb-2">
+            Welcome to NeuroKey Desktop
+          </h2>
+          <p className="text-subText">Your offline-first vault is ready.</p>
+        </div>
+      </div>
+    </SidebarLayout>
   );
 }
 
